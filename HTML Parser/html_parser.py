c07d62bf5d96f671758c6185f31fc4f226ccbc80
Caolán Maguire <caolanmichaelmaguire@gmail.com>
2023-06-29 10:55:23 +0100

    whats your name added

--- HTML Parser/html_parser.py
+++ HTML Parser/html_parser.py
@@ -1,15 +1,21 @@
-from html.parser import HTMLParser
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
 
-# create a subclass and override the handler methods
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print ("Found a start tag  :", tag)
-    def handle_endtag(self, tag):
-        print ("Found an end tag   :", tag)
-    def handle_startendtag(self, tag, attrs):
-        print ("Found an empty tag :", tag)
+i = 0
+while i != n:
+    i += 1
+# from html.parser import HTMLParser
 
-# instantiate the parser and fed it some HTML
-parser = MyHTMLParser()
-parser.feed("<html><head><title>HTML Parser - I</title></head>"
-            +"<body><h1>HackerRank</h1><br /></body></html>")
+# # create a subclass and override the handler methods
+# class MyHTMLParser(HTMLParser):
+#     def handle_starttag(self, tag, attrs):
+#         print ("Found a start tag  :", tag)
+#     def handle_endtag(self, tag):
+#         print ("Found an end tag   :", tag)
+#     def handle_startendtag(self, tag, attrs):
+#         print ("Found an empty tag :", tag)
+
+# # instantiate the parser and fed it some HTML
+# parser = MyHTMLParser()
+# parser.feed("<html><head><title>HTML Parser - I</title></head>"
+#             +"<body><h1>HackerRank</h1><br /></body></html>")
