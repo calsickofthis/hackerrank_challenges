d1af474d411effc19d3983e899a848de6388ab99
Caolán Maguire <caolanmichaelmaguire@gmail.com>
2023-07-03 14:57:09 +0100

    finished alphabet rangoli

--- Alphabet Rangoli/answer.py
+++ Alphabet Rangoli/answer.py
@@ -1,31 +1,27 @@
 import math
 
 def print_rangoli(size):
+    '''print rangoli'''
     alphabet = list(map(chr, range(97, 123)))
     letters = alphabet[0:size][::-1]
 
     length = (len(letters) * 2) -1
     width = (length * 2) - 1
-    
-    i = 0
+    i = 1
     y = 1
 
     output =[]
 
     while i != math.ceil(length / 2) + 1:
-
         line = letters[0:i] + letters[0:i - 1][::-1]
-        
         output.append(line)
-
-        print(*line, sep = "-")
-
+        list_to_str = '-'.join([str(elem) for elem in line])
+        print(list_to_str.center(width,"-"))
         i += 1
         y += 2
     for items in output[::-1][1:]:
-        print(*items, sep = "-")
-        # print(*items.center(width), sep = "-")
-        # print((nex+"-"+c+"-"+d).center(w,"-"))
+        list_to_str = '-'.join([str(elem) for elem in items])
+        print(list_to_str.center(width,"-"))
 
 if __name__ == '__main__':
     n = int(input())
