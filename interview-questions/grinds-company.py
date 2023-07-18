399444cd97a954535b6433145255b0d5d0ee5eb0
Caolán Maguire <caolanmichaelmaguire@gmail.com>
2023-07-18 17:13:26 +0100

    updated file

--- interview-questions/grinds-company.py
+++ interview-questions/grinds-company.py
@@ -1,21 +1,24 @@
 '''anagram calculator'''
 from english_words import get_english_words_set
+
+
 english_dictionary = get_english_words_set(['web2'])
 
-anagram_dictionary = {}
+def anagram_dictionary():
+    anagram_dictionary = {}
 
-for word in english_dictionary:
-    key = ''.join(sorted(word))
-    # print(key)
+    for word in english_dictionary:
+        key = ''.join(sorted(word))
 
-    if key in anagram_dictionary:
-        anagram_dictionary[key].append(word)
+        if key in anagram_dictionary:
+            anagram_dictionary[key].append(word)
 
-    else:
-        # print('Not In Dictionary')
-        anagram_dictionary[key] = []
-        anagram_dictionary[key].append(word)
-        # print(key)
-        # print(anagram_dictionary[key])
-    
-return anagram_dictionary
+        else:
+            # print('Not In Dictionary')
+            anagram_dictionary[key] = []
+            anagram_dictionary[key].append(word)
+    return anagram_dictionary
+
+anagram_dictionary = anagram_dictionary()
+
+print(anagram_dictionary['acer'])
